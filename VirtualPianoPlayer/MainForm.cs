db7e10ba9a17d4d46a51446569caa7b7851974c8
Hayden Andreyka <haydenandreyka@gmail.com>
2018-01-04 19:47:06 -0700

    create project

---
+++ VirtualPianoPlayer/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualPianoPlayer
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.ClientSize = new System.Drawing.Size(511, 306);
+			this.Name = "MainForm";
+			this.Text = "Virtual Piano Player";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
